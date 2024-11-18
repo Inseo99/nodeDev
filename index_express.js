a873daa3454c5c3afa0e90cdc97052b54b03bfb1
inseo99 <jjj990626@naver.com>
2024-11-18 16:24:51 +0900

    게시판 글쓰기화면 추가

--- index_express.js
+++ index_express.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const app = express();
+const app = express();  // Express 애플리케이션을 생성
 const port = 3002;
+
+var dbconn = require(__dirname + "/dbconn.js"); // __dirname = 현재 실행 중인 파일의 디렉토리 경로
+var conn = dbconn.init();
+dbconn.connect(conn);
+
+var bodyparser = require("body-parser");    // 데이터 추출 파서 사용
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));   //url인코딩 깨짐 방지
 
 app.use("/main", express.static(__dirname + "/public"));    // 가상경로와 폴더매칭
 
@@ -13,6 +21,11 @@ app.get("/", (req, res) => {
 //     return;
 //     }
 
+app.post("/boardWriteAction", function(req, res) {
+    var body = req.body;
+    console.log(body);
+});
+
 app.listen(port, () => {
-    console.log("Listen : ${port}");
+    console.log("server running!");
 });
